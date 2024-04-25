fa5cc6010804482dc2cc3292c4b3bea7e7d4e79e
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-04-25 16:11:00 +0200

    Demo

--- MaccatalystLibsodium/AppDelegate.cs
+++ MaccatalystLibsodium/AppDelegate.cs
@@ -1,4 +1,7 @@
-﻿namespace MaccatalystLibsodium;
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MaccatalystLibsodium;
 
 [Register ("AppDelegate")]
 public class AppDelegate : UIApplicationDelegate {
@@ -7,23 +10,69 @@ public class AppDelegate : UIApplicationDelegate {
 		set;
 	}
 
-	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int crypto_aead_aes256gcm_is_available();
+
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void randombytes_buf(out ulong buf, UIntPtr size);
+
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int sodium_init();
+
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int sodium_library_version_major();
+
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int sodium_library_minimal();
+
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int sodium_library_version_minor();
+
+    [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+    private static extern IntPtr sodium_version_string();
+
+    public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 	{
-		// create a new window instance based on the screen size
-		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+        StringBuilder sb = new();
+
+        try
+        {
+            sb.AppendFormat("sodium_version_string: {0}; ", Marshal.PtrToStringAnsi(sodium_version_string()));
+            sb.AppendFormat("sodium_library_version_major: {0}; ", sodium_library_version_major());
+            sb.AppendFormat("sodium_library_version_minor: {0}; ", sodium_library_version_minor());
+            sb.AppendFormat("sodium_library_minimal: {0}; ", sodium_library_minimal());
+            int error = sodium_init();
+            sb.AppendFormat("sodium_init: {0}; ", error);
+            if (error == 0)
+            {
+                randombytes_buf(out ulong buf, (UIntPtr)sizeof(ulong));
+                sb.AppendFormat("randombytes_buf: 0x'{0:X8}'; ", buf);
+                sb.AppendFormat("crypto_aead_aes256gcm_is_available: {0}; ", crypto_aead_aes256gcm_is_available());
+            }
+        }
+        catch (Exception e)
+        {
+            sb.AppendLine(e.StackTrace);
+        }
+
+        // create a new window instance based on the screen size
+        Window = new UIWindow (UIScreen.MainScreen.Bounds);
 
 		// create a UIViewController with a single UILabel
 		var vc = new UIViewController ();
 		vc.View!.AddSubview (new UILabel (Window!.Frame) {
 			BackgroundColor = UIColor.SystemBackground,
 			TextAlignment = UITextAlignment.Center,
-			Text = "Hello, Mac Catalyst!",
+			Text = sb.ToString(),
 			AutoresizingMask = UIViewAutoresizing.All,
 		});
 		Window.RootViewController = vc;
 
 		// make the window visible
 		Window.MakeKeyAndVisible ();
+
+        //Console.WriteLine(sb.ToString().Replace(';', '\n'));
+        //Environment.Exit(0);
 
 		return true;
 	}
---
+++ README.md
@@ -0,0 +1,12 @@
+# How to run?
+
+First, copy your libsodium nuget package to `LocalNugetPackages` folder in this repo. Then just run the application
+
+```bash
+cd MaccatalystLibsodium
+dotnet build && dotnet run -f net8.0-maccatalyst
+```
+
+You should see:
+
+<img width="1136" alt="image" src="https://github.com/jedisct1/libsodium/assets/203266/c07d4ceb-aa8c-471d-b681-7a3b65101c30">
